5a8fb333cc5f5bd3f6db4ec06509de5c860855f5
aku776888-hash <aku776888@gmail.com>
2026-07-18 15:08:14 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://xpitjaicvhbidluhjkvb.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_YEmwUDh1WhXCm8T5GApxag__eC-k5Gq";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
